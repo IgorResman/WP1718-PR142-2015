702e55b3ff1717c138edb4943eb3f08366fc6d77
Igor Resman <igor.resman@gmail.com>
2018-06-27 23:57:59 +0200

    validacija

---
+++ WebProjekat/WebProjekat/Scripts/Login.js
@@ -0,0 +1,25 @@
+﻿
+function validateLogin() {
+    $("#loginForm").validate({
+        rules: {
+            korisnickoIme: {
+                required: true,
+                minlength: 4
+            },
+            lozinka: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            korisnickoIme: {
+                required: "Morate uneti ovo polje",
+                minlength: "Korisnicko ime mora biti minimum 4 karaktera"
+            },
+            lozinka: {
+                required: "Morate uneti ovo polje",
+                minlength: "Lozinka mora biti minimum 4 karaktera"
+            }
+        },
+    });
+}
